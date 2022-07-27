2029a7a9c03793f3e349f1f33451d49b4bad5381
Ziel <frans_421_12@hotmail.com>
2022-07-27 13:58:21 -0500

    Proyecto base y limpio

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    
+
+    <div class="wrapper">
+      
+    </div>
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+
+</style>
