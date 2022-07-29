bbe3d89cd3dc29a95538eeea5cf959c0c5909955
Ziel <frans_421_12@hotmail.com>
2022-07-28 22:34:49 -0500

    feat: Diseño nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,48 @@
 <template>
-    <div>
-        This is my Nav component
+    <div class="Nav container mx-auto my-8 grid grid-flow-col grid-cols-2 gap-4">
+        <div>
+            <img :src="PorscheLogo" alt="Porsche logo" class="w-16 mx-auto md:mx-0" />
+        </div>
+        <div class="hidden md:block">
+            <div class="grid grid-flow-col grid-cols-7 gap-4 items-center h-full">
+                <p v-for="option, optionIndex in menu" :key="optionIndex" @click="openLink(option.link)" class="cursor-pointer text-sm font-bold">{{ option.title }}</p>
+            </div>
+        </div>
+        <div class="block md:hidden fixed right-8 top-8 cursor-pointer">
+            <img :src="MenuIcon" alt="Display menu" class="w-16" />
+        </div>
     </div>
 </template>
 
-<script>
-export default {
-    
+<script setup>
+import MenuIcon from '../assets/menu.png'
+import PorscheLogo from '../assets/porsche_logo.png'
+
+const menu = [{
+    title: 'Brand',
+    link: ''
+}, {
+    title: '718',
+    link: ''
+}, {
+    title: '911',
+    link: ''
+}, {
+    title: 'Taycan',
+    link: ''
+}, {
+    title: 'Macan',
+    link: ''
+}, {
+    title: 'Panamera',
+    link: ''
+}, {
+    title: 'Cayenne',
+    link: ''
+}]
+
+const openLink = (link) => {
+    window.open(link)
 }
 </script>
 
