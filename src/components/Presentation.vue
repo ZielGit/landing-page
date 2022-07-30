86b4c7667336afaf3d5f4406aeb453c3f9f3f47a
Ziel <frans_421_12@hotmail.com>
2022-07-29 19:47:09 -0500

    feat: Presentación parte 1

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
-        This is my Presentation component
+        {{ content }}
     </div>
 </template>
 
 <script>
 export default {
-    
+    props: {
+        content: {
+            default: () => {},
+            type: Object
+        }
+    },
+    setup(props) {
+        console.log(props.content)
+    }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,7 @@
     <div>
         <Nav />
         <MainHeader />
-        <Presentation />
-        <Presentation />
+        <Presentation v-for="obj, objIndex in presentations" :key="objIndex" :content="obj" />
         <Stats />
         <Tabs />
         <Footer />
@@ -17,9 +16,37 @@ import Nav from '../components/Nav.vue'
 import Presentation from '../components/Presentation.vue'
 import Stats from '../components/Stats.vue'
 import Tabs from '../components/Tabs.vue'
+import { reactive, toRefs } from 'vue'
+import PorscheCockpit from '../assets/porsche-cockpit.png'
 
 export default {
-    components: { Nav, MainHeader, Presentation, Stats, Tabs, Footer },
+    components: { 
+        Nav,
+        MainHeader,
+        Presentation,
+        Stats,
+        Tabs,
+        Footer 
+    },
+    setup() {
+        let data = reactive({
+            presentations: [{
+                left: true,
+                colums: [{
+                    backgroundColor: 'customBlue',
+                    title: 'Future is now',
+                    subtitle: 'Taycan concept',
+                    description: 'The soul has many aspects. It is variable and constantly changing. When viewing the Porsche Taycan, it reveals itself by a smile that expresses total delight. Or by the urge for freedom at the wheel of the new Taycan Cross Turismo that holds its ground, even away from everyday life.'
+                }, {
+                    imgSrc: PorscheCockpit
+                }]
+            }]
+        })
+
+        return {
+            ...toRefs(data)
+        }
+    }
 }
 </script>
 
