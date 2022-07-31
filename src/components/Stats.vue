af0b94353769a8342ccd2eab7b75e1ea0c701028
Ziel <frans_421_12@hotmail.com>
2022-07-30 19:11:23 -0500

    feat: Stats parte 1

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,16 +1,20 @@
 <template>
-    <div class="stats">
-        <p>This is my Stats component</p>
-        <stats-card />
+    <div class="Stats bg-cover bg-no-repeat bg-bottom" :style="`background-image:url(${PorscheBackground}); height: 450px;`">
+        <div class="w-full h-full bg-customBlue opacity-75 absolute z-0" />
+        <div class="Stats-Components z-50">
+          <p class="text-white">some text</p>
+        </div>
+        <!-- <stats-card /> -->
     </div>
 </template>
 
-<script>
-import StatsCard from './elements/stats/StatsCard.vue'
-export default {
-  components: { StatsCard },
+<script setup>
+import PorscheBackground from '../assets/2020-porsche.jpeg'
+// import StatsCard from './elements/stats/StatsCard.vue'
+// export default {
+//   components: { StatsCard },
     
-}
+// }
 </script>
 
 <style lang="scss" scoped>
