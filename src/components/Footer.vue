d13f427be4259b297dccc7e6c0bbdf6e02356273
Ziel <frans_421_12@hotmail.com>
2022-07-31 20:51:56 -0500

    feat: Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,9 @@
 <template>
-    <div>
-        This is my Footer components
+    <div class="Footer flex items-center justify-center my-16 mb-32">
+        <img :src="PorscheLogo" alt="Porsche logo" class="w-32 mx-auto md:mx-0" />
     </div>
 </template>
 
-<script>
-export default {
-    
-}
+<script setup>
+import PorscheLogo from '../assets/porsche_logo.png'
 </script>
-
-<style lang="scss" scoped>
-
-</style>
